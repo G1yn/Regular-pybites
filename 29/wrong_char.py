174c17e60f5a02bb2702b695aac57e54fcdb0572
Glyn Davies <3874352+G1yn@users.noreply.github.com>
2021-12-01 09:46:40 +0000

    Convert alphanumeric string to list first

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,21 +1,15 @@
 def get_index_different_char(chars):
-    alphanumeric = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    mydict = {}
-    alpha = 0
-    nonalpha = 0
+    alphanumeric = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+    
     alphapos = []
     nonalphapos = []
     for mynum, mychar in enumerate(chars):
-        if len(str(mychar)):
-            if str(mychar) in alphanumeric:
-                alphapos.append(mynum)
-                alpha += 1
-            else:
-                nonalphapos.append(mynum)
-                nonalpha += 1
+        if str(mychar) in alphanumeric:
+            alphapos.append(mynum)
+        else:
+            nonalphapos.append(mynum)
 
-    if alpha < nonalpha:
+    if len(alphapos) == 1:
         return alphapos[0]
     else:
         return nonalphapos[0]
-
