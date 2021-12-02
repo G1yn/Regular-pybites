e7ce84dd292e0ab7bc624cebd6c7a05ebe2f3fc5
Glyn Davies <3874352+G1yn@users.noreply.github.com>
2021-12-02 18:15:54 +0000

    Mover .upper() out of loop

--- 44/license.py
+++ 44/license.py
@@ -3,9 +3,9 @@ import string
 
 
 def gen_key(parts = 4, chars_per_part = 8):
-    alphabet = string.ascii_letters + string.digits
+    alphabet = string.ascii_letters.upper() + string.digits
     key = ''
     for i in range(parts):
-        key += ''.join(secrets.choice(alphabet).upper() for i in range(chars_per_part)) + '-'
+        key += ''.join(secrets.choice(alphabet) for i in range(chars_per_part)) + '-'
     key.rstrip("-")
     return key[:-1]
