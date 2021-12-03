62c0c78d6c870ad47a4da8209026d11d8e5a21c1
Glyn Davies <3874352+G1yn@users.noreply.github.com>
2021-12-03 19:25:32 +0000

    changed to use elif

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,9 +1,9 @@
 def fizzbuzz(num):
     if num % 3 == 0 and num % 5 == 0:
         return 'Fizz Buzz'
-    if num % 3 == 0 :
+    elif num % 3 == 0 :
         return 'Fizz'
-    if num % 5 == 0:
+    elif num % 5 == 0:
         return 'Buzz'
     else:
         return num
