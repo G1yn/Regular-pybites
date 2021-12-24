5ed2d944e2cdf4dcc50089eb052b85158446b0f9
Glyn Davies <3874352+G1yn@users.noreply.github.com>
2021-12-24 19:12:32 +0000

    Using shortcut for symmetric difference

--- 77/uncommon.py
+++ 77/uncommon.py
@@ -1,8 +1,13 @@
 def uncommon_cities(my_cities, other_cities):
     """Compare my_cities and other_cities and return the number of different
        cities between the two"""
-    myset = set(my_cities)
-    otherset = set(other_cities)
-    myset.symmetric_difference_update(otherset)
-    return len(myset)
+   #  myset = set(my_cities)
+   #  otherset = set(other_cities)
+   #  myset.symmetric_difference_update(otherset)
+   #  return len(myset)
+
+# Note that there are shortcuts for the operators with ^ being symmetric difference
+# Hence can use:
+
+    return len(set(my_cities) ^ set(other_cities))
     
