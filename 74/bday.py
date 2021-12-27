152273067da247a92c1e1befb7fc359b9619fa3b
Glyn Davies <3874352+G1yn@users.noreply.github.com>
2021-12-27 18:46:50 +0000

    added note and more pythonic

--- 74/bday.py
+++ 74/bday.py
@@ -3,4 +3,9 @@ import calendar
 
 def weekday_of_birth_date(date):
     """Takes a date object and returns the corresponding weekday string"""
-    return calendar.day_name[calendar.weekday(date.year, date.month, date.day)]
+    return calendar.day_name[date.weekday()]
+
+
+# def weekday_of_birth_date(date):
+#     """Takes a date object and returns the corresponding weekday string"""
+#     return calendar.day_name[calendar.weekday(date.year, date.month, date.day)]
